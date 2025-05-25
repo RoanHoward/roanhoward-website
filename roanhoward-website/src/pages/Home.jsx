83d594a3426d31927541a044165c7dbec7d06d69
roan <roanahoward@gmail.com>
2025-05-24 17:25:19 -0700

    formatting and pargraph update

--- roanhoward-website/src/pages/Home.jsx
+++ roanhoward-website/src/pages/Home.jsx
@@ -6,12 +6,15 @@ export default function Home() {
     <div className="home-container">
       <div className="profile-block">
         <img className="profile-image" src="/Images/myface.jpg" alt="Roan Howard" />
-        <h1 className="name">Roan Howard</h1>
+        <h1 className="name">RoanBot</h1>
     </div>
       <div className="about-section">
-        <h2 className="about-header">About Me</h2>
         <p className="about-text">
-          I’m a computer science student at Stanford with a passion for building tools, editing film, and exploring ideas that blend technology with Indigenous identity. This site is where I share my work and story.
+          Aaskuwut eesh yóo xát duwasáakw. Yéil áyá xát. K’ineix Kwáan naax xát sitee. Tsisk’w hítdáx áyá xát. <br /><br /> My name is Roan Howard, and my Tlingit name is Aaskuwut eesh.
+          I am Alaskan Native, Raven moiety, of the People of the Copper River Clan from the Owl House. I spent my childhood in a tree-lined suburb of Kirkland, Washington, and now reside in California, where I study and create. <br /> <br />
+          I’m a 20-year-old junior at Stanford University, majoring in Computer Science with a concentration in Artificial Intelligence and minoring in Art Practice.
+          I am a scrappy idea hopper and thrive in the spaces where my interests intersect — where creativity, culture, and technology come together.<br /> <br />
+          This site is where I share both my work and my story. There’s a lot more than I can write here, so feel free to ask my AI clone, Roanbot.<br />
         </p>
       </div>
     </div>
